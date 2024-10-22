1e0a1217bd3835d254f397e64bf4420a7ffa2a45
PetjaPajunen <137160157+PetjaPajunen@users.noreply.github.com>
2024-10-23 01:45:58 +0300

    Add files via upload

---
+++ Moduuli 9/Tehtava1.py
@@ -0,0 +1,17 @@
+
+class auto:
+    def __init__(self, rekisteritunnus, huippunopeus):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.tamanhetkinen_nopeus = 0
+        self.kuljettu_matka = 0
+
+    def __str__(self) -> str:
+        return (f'Rekisteritunnus: {self.rekisteritunnus},\n'
+                f'Huippunopeus: {self.huippunopeus} km/h,\n'
+                f'Tämänhetkinen nopeus: {self.tamanhetkinen_nopeus} km/h,\n'
+                f'Kuljettu matka: {self.kuljettu_matka} km')
+    
+if __name__ == "__main__":
+    auto = auto("ABC-123", 142)
+    print(auto)
